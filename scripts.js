83589c66e8158101a5b81bdca8199de120bf88a5
FelhageDike <tavroslive@mail.ru>
2021-07-16 04:20:28 +0300

    task_3; using function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-let numberOfFilms = prompt("Сколько фильмов ты посмотрел?", "18");
-console.log(numberOfFilms);
+let numberOfFilms;
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -9,11 +8,47 @@ let personalMovieDB = {
     privat: false,
 
 };
-console.log(personalMovieDB);
-let a = prompt("Какой был последний фильм?", "");
-let b = prompt("Оцени его?", "");
-let c = prompt("Какой был последний фильм?", "");
-let d = prompt("Оцени его?", "");
+function showMyDB(){
+    if(personalMovieDB.privat== false){
+        console.log(personalMovieDB);
+    }
+}
+function writeYourGenres() {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1]= prompt(`Favorite Generes ${i}`, "");
+    }
+}
+ function start(){
+     numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "18");
+     while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+         let numberOfFilms = prompt("Сколько фильмов ты посмотрел?", "18");
+     }
+ }
+ function personalLevel(){
+ switch (true){
+    case (numberOfFilms < 10):
+        alert("Что то маловато фильмов дружок пирожок");
+        break;
+    case (numberOfFilms>10 && numberOfFilms<30):
+        alert("Ты норм поцанчик");
+        break;
+    case (numberOfFilms>30):
+        alert("Задрот...");
+        break;
+    default:alert("Даже не знаю кто ты лох....")
+}
+ }
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Какой был последний фильм?", "");
+        let b = prompt("Оцени его?", "");
+        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+}
+writeYourGenres();
+showMyDB();
